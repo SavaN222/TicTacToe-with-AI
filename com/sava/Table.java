d46c086513503b234c8bfb32449fa43587b6eb7f
SavaN22 <savanakarada22@gmail.com>
2020-12-14 10:04:17 +0100

    Initialise field amd draw field

--- com/sava/Table.java
+++ com/sava/Table.java
@@ -7,7 +7,30 @@ public class Table {
     private static final int col = 3;
     public static int[][] field = new int[row][col];
     private static Scanner sc = new Scanner(System.in);
-    public Table() {
 
+    public Table() {
+        System.out.print("Enter the cells: ");
+        char[] cells = sc.nextLine().toCharArray();
+        int num = 0;
+        while (num < 9) {
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (cells[num] == '_') {
+                        cells[num] = ' ';
+                    }
+                    field[i][j] = cells[num];
+                    num++;
+                }
+            }
+        }
+        drawField();
+    }
+
+    public void drawField() {
+        System.out.println("---------");
+        for (int i = 0; i < row; i++) {
+            System.out.printf("| %c %c %c |\n", field[i][0], field[i][1], field[i][2]);
+        }
+        System.out.println("---------");
     }
 }
