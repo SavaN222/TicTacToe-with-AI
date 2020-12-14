cb246cae0d7c2b97cfa44d2209accc1f4ebac8c8
SavaN22 <savanakarada22@gmail.com>
2020-12-14 09:56:57 +0100

    Started files and git initialisation

---
+++ com/sava/Game.java
@@ -0,0 +1,4 @@
+package com.sava;
+
+public class Game {
+}
---
+++ com/sava/Table.java
@@ -0,0 +1,13 @@
+package com.sava;
+
+import java.util.Scanner;
+
+public class Table {
+    private static final int row = 3;
+    private static final int col = 3;
+    public static int[][] field = new int[row][col];
+    private static Scanner sc = new Scanner(System.in);
+    public Table() {
+
+    }
+}
